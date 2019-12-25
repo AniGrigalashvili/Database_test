84ac736e691d256ab3b49502be53dc6d8eff2595
aniko <anogrigalashvili@gmail.com>
2019-12-25 15:53:50 +0400

    commit

---
+++ dbo/Stored Procedures/addperson.sql
@@ -0,0 +1,21 @@
+﻿
+
+CREATE procedure [dbo].[addperson](
+       @Name nchar(10)
+      ,@LastName nchar(10)
+      ,@BirthDate datetime2(7) = null
+	  ,@result nvarchar(100) output
+)
+as
+begin
+if (exists(select * from dbo.Persons where [Name]=@Name))
+begin
+  set @result = 'already exists'
+return 
+end
+
+insert dbo.Persons
+values(@Name,@LastName,@BirthDate)
+
+  set @result = 'person Added'
+end
---
+++ dbo/Tables/Persons.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Persons] (
+    [Id]        INT           IDENTITY (1000, 1) NOT NULL,
+    [Name]      NCHAR (10)    NULL,
+    [LastName]  NCHAR (10)    NULL,
+    [BirthDate] DATETIME2 (7) CONSTRAINT [DF_Persons_BirthDate] DEFAULT (getdate()) NULL
+);
+
