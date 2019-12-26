27f9233056ca5f924e7742e8c1de03cda416d846
aniko <anogrigalashvili@gmail.com>
2019-12-26 17:40:46 +0400

    commit

---
+++ dbo/Tables/Students.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Students]
+(
+    [StudentId] INT NOT NULL PRIMARY KEY,
+    [F_Name] NVARCHAR(50) NOT NULL,
+    [L_Name] NVARCHAR(50) NOT NULL,
+	[BirthDate] [datetime2](7) NULL,
+    [Phone] NVARCHAR(50) NULL
+)
